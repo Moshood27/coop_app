081ff56e015875b79d7d7fb0cb69daa5b08f6635
moshood <madewale5@gmail.com>
2026-06-03 16:03:10 +0100

    DEPLOY: Clean build with all assets

--- backend/config/database.php
+++ backend/config/database.php
@@ -62,7 +62,7 @@ return [
                 (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
             'dump' => [
-                'add_extra_option' => '--skip-column-statistics --skip-ssl',
+                'add_extra_option' => '--skip-ssl',
                 'use_single_transaction' => true,
             ],
         ],
