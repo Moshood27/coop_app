e860a23343e2e3a36134de2bb07ea106f9bf1414
moshood <madewale5@gmail.com>
2026-08-04 12:21:06 +0100

    update

---
+++ frontend/dist/assets/esm-CzIvHak2.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BnRxlWLV.js","assets/index-Rc2XEjAC.js","assets/axios-BYS4mTg9.js","assets/runtime-core.esm-bundler-CqfLSHS2.js","assets/http-Coj0wk6I.js"])))=>i.map(i=>d[i]);
+import{T as e,b as t}from"./index-Rc2XEjAC.js";var n=e(`App`,{web:()=>t(()=>import(`./web-BnRxlWLV.js`).then(e=>new e.AppWeb),__vite__mapDeps([0,1,2,3,4]))});export{n as App};
