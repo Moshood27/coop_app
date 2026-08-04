4718a23bd893d24f1940e8ad2dc0f2797ec6d856
moshood <madewale5@gmail.com>
2026-08-04 18:01:41 +0100

    Update new records

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -73,8 +73,8 @@ return [
 
     'middleware' => [
         'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
-        'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
-        'validate_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
+        'encrypt_cookies' => \Illuminate\Cookie\Middleware\EncryptCookies::class,
+        'validate_csrf_token' => \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
     ],
 
     // Disable Sanctum's automatic last_used_at updates so we can manage idle timeout manually
